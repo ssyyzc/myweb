876647b82e49dd14a1608aa1ea63e929e22b38e2
ssyyzc_001 <ssyyzc_001@163.com>
2023-02-09 13:48:01 +0800

    2

--- www/test.php
+++ www/test.php
@@ -3,14 +3,15 @@
 // // redis 测试
 $redis = new Redis();
 $redis->connect('myredis',6379);
-echo "redis is running: ".$redis->ping();
+echo "redis is running: ";
+var_dump($redis->ping());
 echo '-------------------<br>';
 
 // mysql测试  docker新键的数据库中，需要自行新建数据库和表，这里已创建fastadmin库
 // 连接的host要注意，与./nginx/default.conf中的php连接一样，不是ip，是docker-compose中配置的容器名，container_name
 
 try {
-    $conn = new PDO('mysql:host=mymysql;dbname=fastadmin;port=3306', 'root', 'root');
+    $conn = new PDO('mysql:host=mymysql;dbname=test_base;port=3306', 'root', '123456');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);// 设置 PDO
 
     // $conn->exec("CREATE TABLE if not exists author (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,name VARCHAR(30) NOT NULL,lastname VARCHAR(30) NOT NULL,email VARCHAR(50))"); // 创建表
